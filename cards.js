9c783f6bb9588370cf0bdc35d84a70e003cb2b7c
Shivam <98345270+Shivam-Brahmkshatriya@users.noreply.github.com>
2023-02-23 00:33:42 +0530

    Add files via upload

---
+++ cards.js
@@ -0,0 +1,112 @@
+const express = require("express");
+const user_jwt = require("../middleware/user_jwt");
+
+const Cards = require('../models/Cards');
+
+const router = express.Router();
+
+//desc      Create new cards
+// method POST
+router.post('/', user_jwt, async (req, res, next) => {
+    try {
+       const Cards1 = await Cards.create({ title: req.body.title, description: req.body.description, user: req.user.id});
+       if(!Cards1) {
+            return res.status(400).json({
+                success: false,
+                msg: 'Something went wrong'
+            });
+       }
+
+       res.status(200).json({
+        success: true,
+        cards: Cards1,
+        msg: 'Successfully Created.'
+    });
+    } catch (error) {
+        next(error);
+    }
+});
+
+
+//desc Fetch all cards
+//methods GET
+
+router.get('/', user_jwt, async(req, res, next) => {
+    try {
+        const Cards2 = await Cards.find({user: req.user.id, finished: false});
+        if(!Cards2) {
+            return res.status(400).json({ success: false, msg: 'Something error happend'});
+        }
+
+        res.status(200).json({ success: true, count:Cards2.length, cards: Cards2,
+            msg: 'Successfully Fetched.' })
+    } catch (error) {
+        next(error);
+    }
+});
+
+//desc Fetch all Finished cards
+//methods GET
+
+router.get('/finished', user_jwt, async(req, res, next) => {
+    try {
+        const Cards2 = await Cards.find({user: req.user.id, finished: true});
+        if(!Cards2) {
+            return res.status(400).json({ success: false, msg: 'Something error happend'});
+        }
+
+        res.status(200).json({ success: true, count:Cards2.length, cards: Cards2,
+            msg: 'Successfully Fetched.' })
+    } catch (error) {
+        next(error);
+    }
+});
+
+//desc Update all cards
+//methods POST
+
+router.put('/:id', async(req, res, next) => {
+    try {
+        let Cards3 = await Cards.findById(req.params.id);
+        if(!Cards3){
+            return res.status(400).json({success: false, msg: 'Task Card not exits'});
+        }
+        Cards3 = await Cards.findByIdAndUpdate(req.params.id, req.body ,{
+            new: true, 
+            runValidators: true
+        });
+
+        // if(!Cards3) {
+        //     return res.status(400).json({success: false, msg: 'Some thong went wrong'});
+        // }
+        res.status(200).json({success: true, cards: Cards3, msg: 'Successfully updated'});
+        
+    } catch (error) {
+        next(error);
+    }
+});
+
+//desc DELETE all cards
+//methods DELETE
+
+router.delete('/:id', async (req, res, next) => {
+
+    try {
+        let Cards3 = await Cards.findById(req.params.id);
+        if(!Cards3){
+            return res.status(400).json({success: false, msg: 'Task Card not exits'});
+        }
+
+        Cards3 = await Cards.findByIdAndDelete(req.params.id);
+
+        res.status(200).json({
+            success:true, msg: 'Successfully Deleted CArds'
+        });
+        
+    } catch (error) {
+        next(error);
+    }
+    
+});
+
+module.exports = router;
